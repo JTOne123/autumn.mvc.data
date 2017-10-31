539bbcce0566eea3f2650bc7485a597f25717b1c
gwendall <gwendall@gwendall-msi.garnier-family.lan>
2017-10-31 08:20:56 +0100

    add Post/Delete/Put controller

--- src/Autumn.Data.Rest/Controllers/RestRepositoryController.cs
+++ src/Autumn.Data.Rest/Controllers/RestRepositoryController.cs
@@ -25,7 +25,7 @@ namespace Autumn.Data.Rest.Controllers
         }
 
         [HttpGet("{id}")]
-        public async Task<IActionResult> FindById(TId id)
+        public virtual async Task<IActionResult> FindById(TId id)
         {
             var result = await _repository.FindOneAsync(id);
             if (result == null) return NotFound();
@@ -34,12 +34,37 @@ namespace Autumn.Data.Rest.Controllers
 
 
         [HttpGet("")]
-        public async Task<IActionResult> Find(Expression<Func<TU, bool>> filter, IPageable pageable)
+        public virtual async Task<IActionResult> Find(Expression<Func<TU, bool>> filter, IPageable pageable)
         {
             var result = await _repository.FindAsync(filter, pageable);
             return result.TotalElements == result.NumberOfElements
                 ? Ok(result)
                 : StatusCode((int) HttpStatusCode.PartialContent, result);
+        }
+
+        [HttpPost]
+        public virtual async Task<IActionResult> Post([FromBody] TU entity)
+        {
+            var result = await _repository.CreateAsync(entity);
+            return Created(string.Empty, result);
+        }
+        
+        [HttpDelete("{id}")]
+        public virtual async Task<IActionResult> Delete(TId id)
+        {
+            var result = await _repository.FindOneAsync(id);
+            if (result == null) return NoContent();
+            result = await _repository.DeleteAsync(id);
+            return Ok(result);
+        }
+
+        [HttpPut("{id}")]
+        public virtual async Task<IActionResult> Put([FromBody] TU entity, TId id)
+        {
+            var result = await _repository.FindOneAsync(id);
+            if (result == null) return NoContent();
+            result = await _repository.UpdateAsync(entity, id);
+            return Ok(result);
         }
     }
 }
