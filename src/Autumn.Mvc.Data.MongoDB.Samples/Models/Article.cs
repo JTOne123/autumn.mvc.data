303f0a4382753396ff4f2032369fe522a0a24fe3
gwendall <gwendall@gwendall-msi.garnier-family.lan>
2017-11-09 21:06:24 +0100

    test date

--- src/Autumn.Mvc.Data.MongoDB.Samples/Models/Article.cs
+++ src/Autumn.Mvc.Data.MongoDB.Samples/Models/Article.cs
@@ -1,16 +1,25 @@
-﻿using Autumn.Mvc.Data.Models;
+﻿using System;
+using Autumn.Mvc.Data.Models;
 using MongoDB.Bson.Serialization.Attributes;
 
 namespace Autumn.Mvc.Data.MongoDB.Samples.Models
 {
     [BsonIgnoreExtraElements]
-    [Entity(Name = "articles")]
+    [Entity(Name = "articles",Version = "v1")]
     public class Article : AbstractEntity
     {
         [BsonElement("title")]
         public string Title { get; set; }
 
-        [BsonElement("article")]
+        [BsonElement("content")]
         public string Content { get; set; }
+    }
+    
+    [BsonIgnoreExtraElements]
+    [Entity(Name = "articles",Version = "v2")]
+    public class ArticleV2 : Article
+    {
+        [BsonElement("publish_date")]
+        public DateTime? PublishDate { get; set; }
     }
 }
