dae8f2a3ae0595e21394d2ac059495a37d7213a7
gwendallg <gwendall.garnier.fr@gmail.com>
2017-11-29 00:30:28 +0100

    update ci to push nuget.org

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -13,3 +13,8 @@ dotnet pack --configuration Release --output ../../nupkgs /p:PackageVersion=$VER
 
 echo 'nuget push'
 dotnet nuget push ./nupkgs/Autumn.Mvc.Data.$VERSION.nupkg -k $API_KEY -s $SOURCE
+dotnet nuget push ./nupkgs/Autumn.Mvc.Data.MongoDB.$VERSION.nupkg -k $API_KEY -s $SOURCE
+dotnet nuget push ./nupkgs/Autumn.Mvc.Data.EF.SqlServer.$VERSION.nupkg -k $API_KEY -s $SOURCE
+dotnet nuget push ./nupkgs/Autumn.Mvc.Data.EF.Mysql.$VERSION.nupkg -k $API_KEY -s $SOURCE
+dotnet nuget push ./nupkgs/Autumn.Mvc.Data.EF.Sqlite.$VERSION.nupkg -k $API_KEY -s $SOURCE
+dotnet nuget push ./nupkgs/Autumn.Mvc.Data.EF.Npgsql.$VERSION.nupkg -k $API_KEY -s $SOURCE
