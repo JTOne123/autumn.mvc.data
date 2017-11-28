db469e1f0627eca2ed4566a1e2c99556678c9270
gwendallg <gwendall.garnier.fr@gmail.com>
2017-11-28 23:55:06 +0100

    update ci to push nuget.org

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,4 +5,5 @@ SOURCE=$2
 VERSION=$3
 VERSION_PREFIX=$4
 
-dotnet pack --version-suffix $VERSION_PREFIX --output nupkgs /p:PackageVersion=$VERSION ./src/Autumn.Mvc.Data/Autumn.Mvc.Data.csproj 
+dotnet pack --configuration Release --version-suffix $VERSION_PREFIX --output nupkgs /p:PackageVersion=$VERSION ./src/Autumn.Mvc.Data/Autumn.Mvc.Data.csproj
+ls -l nupkgs/
