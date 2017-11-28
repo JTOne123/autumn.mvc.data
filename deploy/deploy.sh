fdde98117072ae1b4351aeac31f69b343696e5d8
gwendallg <gwendall.garnier.fr@gmail.com>
2017-11-29 00:14:27 +0100

    update ci to push nuget.org

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,7 +3,12 @@ echo 'test'
 API_KEY=$1
 SOURCE=$2
 VERSION=$3
-VERSION_PREFIX=$4
 
-dotnet pack --configuration Release --version-suffix $VERSION_PREFIX --output ../../nupkgs /p:PackageVersion=$VERSION ./src/Autumn.Mvc.Data/Autumn.Mvc.Data.csproj
+dotnet pack --configuration Release --output ../../nupkgs /p:PackageVersion=$VERSION ./src/Autumn.Mvc.Data/Autumn.Mvc.Data.csproj
+dotnet pack --configuration Release --output ../../nupkgs /p:PackageVersion=$VERSION ./src/Autumn.Mvc.Data.MongoDB/Autumn.Mvc.Data.MongoDB.csproj
+dotnet pack --configuration Release --output ../../nupkgs /p:PackageVersion=$VERSION ./src/Autumn.Mvc.Data.EF.SqlServer/Autumn.Mvc.Data.EF.SqlServer.csproj
+dotnet pack --configuration Release --output ../../nupkgs /p:PackageVersion=$VERSION ./src/Autumn.Mvc.Data.EF.Mysql/Autumn.Mvc.Data.EF.Mysql.csproj
+dotnet pack --configuration Release --output ../../nupkgs /p:PackageVersion=$VERSION ./src/Autumn.Mvc.Data.EF.Sqlite/Autumn.Mvc.Data.EF.Sqlite.csproj
+dotnet pack --configuration Release --output ../../nupkgs /p:PackageVersion=$VERSION ./src/Autumn.Mvc.Data.EF.Npgsql/Autumn.Mvc.Data.EF.Npgsql.csproj
+
 ls -l ./nupkgs
