aec212f03855c39e35c711c3ad2448cf32328d7c
gwendallg <gwendall.garnier.fr@gmail.com>
2017-12-09 18:15:26 +0100

    remove antlr

--- tools/rsql/generate-source.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-echo '--------------------------------------------'
-echo ' source generation C# - rsql                ' 
-echo '--------------------------------------------'
-java -jar antlr-4.6-complete.jar AutumnQuery.g -o ../../src/Autumn.Mvc.Data/Models/Queries -Dlanguage=CSharp -listener -encoding UTF-8 -visitor -package Autumn.Mvc.Data.Models.Queries
-
-
